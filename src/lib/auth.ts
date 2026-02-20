6ca41a2147352de922aae583fcb219a56ad99f1e
mintani <mi_gaming@outlook.jp>
2026-02-20 13:51:35 +0000

    chore: remove better-auth and remove unuse files

--- src/lib/auth.ts
+++
@@ -1,9 +0,0 @@
-import { authClient } from "./auth/auth-client";
-
-export const signInWithGoogle = async () => {
-  await authClient.signIn.social({ provider: "google", callbackURL: "/" });
-};
-
-export const signOut = async () => {
-  await authClient.signOut();
-};
