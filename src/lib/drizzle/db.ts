1830724f2d63f0027c80ebabdc652d4b3bceafe1
mintani <mi_gaming@outlook.jp>
2026-02-20 13:39:19 +0000

    docs: delete claude skills

--- src/lib/drizzle/db.ts
+++
@@ -1,9 +0,0 @@
-import { getCloudflareContext } from "@opennextjs/cloudflare";
-import { drizzle } from "drizzle-orm/d1";
-import { cache } from "react";
-import * as schema from "./schema";
-
-export const getDb = cache(async () => {
-  const { env } = await getCloudflareContext({ async: true });
-  return drizzle(env.DB, { schema });
-});
